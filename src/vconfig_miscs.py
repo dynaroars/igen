ecc08a4229c901d5351f2c273c617f7c7ede730d
ThanhVu (Vu) Nguyen @ GiaoChi <20884260+nguyenthanhvuh@users.noreply.github.com>
2015-05-27 16:42:27 -0400

    today's work,  everything should work now as before

--- src/vconfig_miscs.py
+++ src/vconfig_miscs.py
@@ -1,6 +1,7 @@
 import itertools
 import os.path
 import random
+from functools import total_ordering
 from collections import OrderedDict
 
 import z3
@@ -130,6 +131,8 @@ class Dom(OrderedDict):
     log: 0,1
     chunks: 0,2048,4096,65536
 
+    >>> assert dom.siz == 256
+
     >>> dom.z3db
     OrderedDict([('listen', (listen, {'1': 1, '0': 0, 'typ': listen})), ('timeout', (timeout, {'1': 1, '0': 0, 'typ': timeout})), ('ssl', (ssl, {'1': 1, '0': 0, 'typ': ssl})), ('local', (local, {'1': 1, '0': 0, 'typ': local})), ('anon', (anon, {'1': 1, '0': 0, 'typ': anon})), ('log', (log, {'1': 1, '0': 0, 'typ': log})), ('chunks', (chunks, {'0': 0, '65536': 65536, 'typ': chunks, '4096': 4096, '2048': 2048}))])
 
@@ -170,7 +173,10 @@ class Dom(OrderedDict):
     >>> assert len(dom.gen_configs_full()) == 256
 
     >>> random.seed(0)
-    >>> assert len(dom.gen_configs_rand(256)) == 163
+    >>> assert len(dom.gen_configs_rand(256)) == 256
+
+    >>> random.seed(0)
+    >>> assert len(dom.gen_configs_rand(200)) == 140
 
     >>> random.seed(0)
     >>> assert len(dom.gen_configs_tcover1()) == 4
@@ -179,17 +185,22 @@ class Dom(OrderedDict):
     >>> assert len(dom.gen_configs_tcover(3,1,'/tmp/')) == 24
     vconfig_miscs:Debug:cmd: cover -r 0 -s 1 -o  /tmp/casaOutput.txt /tmp/casaInput.txt
 
+
     """
     def __init__(self,d):
         OrderedDict.__init__(self,d)
 
         if CM.__vdebug__:
-            assert all(isinstance(vn,str) and
-                       is_valset(vs) for vn,vs in self.iteritems())
+            assert (self and
+                    all(isinstance(vn,str) and
+                        is_valset(vs) for vn,vs in self.iteritems()))
         
     def __str__(self):
         return '\n'.join("{}: {}".format(k,str_of_valset(vs))
                          for k,vs in self.iteritems())
+    @property
+    def siz(self):
+        return CM.vmul(len(vs) for vs in self.itervalues())
 
     @staticmethod
     def get_dom(dom_file):
@@ -232,17 +243,19 @@ class Dom(OrderedDict):
     #generate configs
     def gen_configs_full(self):
         ns,vs = itertools.izip(*self.iteritems())
-        configs = [HDict(zip(ns,c)) for c in itertools.product(*vs)]
-        return Configs(map(Config,configs))
+        configs = Configs(Config(zip(ns,c)) for c in itertools.product(*vs))
+        return configs
 
     def gen_configs_rand(self,n):
         if CM.__vdebug__:
-            assert n > 0,n
+            assert n > 0, n
 
+        if n >= self.siz:
+            return self.gen_configs_full()
+        
         rgen = lambda: [(k,random.choice(list(dom[k]))) for k in self]
-        configs =  list(set(HDict(rgen()) for _ in range(n)))
-
-        return Configs(map(Config,configs))
+        configs =  Configs.uniq(Config(rgen()) for _ in range(n))
+        return configs
 
     def gen_configs_tcover1(self):
         """
@@ -262,21 +275,21 @@ class Dom(OrderedDict):
                     v = random.choice(list(self[k]))
 
                 config.append((k,v))
-            return HDict(config)
+            return Config(config)
 
-        configs = []
+        configs = Configs()
         while dom_used:
             configs.append(mk())
 
-        return Configs(map(Config,configs))
+        return configs
 
     def gen_configs_tcover(self,cover_siz,tseed,tmpdir):
             
         vals = map(list,self.values())
         vs = Miscs.mk_tcover(vals,cover_siz,tseed,tmpdir)
-        configs = [HDict(zip(self.keys(),vs_)) for vs_ in vs]
+        configs = Configs(Config(zip(self.keys(),vs_)) for vs_ in vs)
 
-        return Configs(map(Config,configs))
+        return configs
 
 
 class Config(HDict):
@@ -294,8 +307,13 @@ class Config(HDict):
         log == 1,
         chunks == 2048)
 
+    >>> assert c1.implies({'listen':'1','chunks':frozenset(['0','2048'])})
+    >>> assert not c1.implies({'listen':'1','chunks':frozenset(['0','4096'])})
+    >>> assert not c1.implies({'boo':'1','chunks':frozenset(['0','2048'])})
+    >>> assert not c1.implies({'listen':'3','chunks':frozenset(['0','2048'])})
+
     """
-    def __init__(self,c):
+    def __init__(self,c=HDict()):
         HDict.__init__(self,c)
 
         if CM.__vdebug__:
@@ -315,6 +333,12 @@ class Config(HDict):
 
         return z3util.myAnd(f)
 
+    def implies(self,d):
+        try:
+            return all(self[k] in d[k] for k in d)
+        except KeyError:
+            return False
+
 
 class Configs(list):
     """
@@ -327,6 +351,7 @@ class Configs(list):
     0. listen=1 timeout=1 ssl=1 local=0 anon=0 log=1 chunks=2048
     1. listen=0 timeout=1 ssl=1 local=0 anon=0 log=0 chunks=0
 
+    >> assert configs.implies({'timeout':frozenset(['1']),'chunks':frozenset(['0','2048'])})
 
     >>> print configs.z3expr(dom.z3db)
     Or(And(listen == 1,
@@ -344,8 +369,10 @@ class Configs(list):
            log == 0,
            chunks == 0))
 
+    >>> assert len(Configs.uniq([c1,c2,c2,c1])) == 2
+
     """
-    def __init__(self,configs):
+    def __init__(self,configs=[]):
         list.__init__(self,configs)
 
         if CM.__vdebug__:
@@ -365,35 +392,32 @@ class Configs(list):
             return '\n'.join("{}. {}".format(i,c)
                              for i,c in enumerate(self))
 
+    def implies(self,d):
+        return all(c.implies(d) for c in self)
+    
     def z3expr(self,z3db):
         return z3util.myOr([c.z3expr(z3db) for c in self])
 
-
-class Core(object):
+    @staticmethod
+    def uniq(configs):
+        return Configs(set(configs))
+    
+class Core(HDict):
     """
-    >>> c1 = Core(HDict())
+    >>> c1 = Core()
     >>> print c1
     true
 
-    >>> c2 = Core(None)
-    >>> print c2
-    None
-
-    >>> c3 = Core(HDict([('listen',frozenset(['1'])),('ssl',frozenset(['1'])),('anon',frozenset(['0'])),('chunks',frozenset(['2048','0']))]))
+    >>> c3 = Core([('listen',frozenset(['1'])),('ssl',frozenset(['1'])),('anon',frozenset(['0'])),('chunks',frozenset(['2048','0']))])
     >>> print c3
     listen=1 ssl=1 anon=0 chunks=0,2048
 
+    >>> assert not c1
+    >>> assert c3
 
-    >>> c3b = Core(HDict([('listen',frozenset(['1'])),('ssl',frozenset(['1'])),('anon',frozenset(['0'])),('chunks',frozenset(['2048','0']))]))
-
-    >>> assert c1 != c2 and c2 != c3 and c3 != c1 and c3 == c3b and hash(c3) == hash(c3b)
+    >>> c3b = Core([('listen',frozenset(['1'])),('ssl',frozenset(['1'])),('anon',frozenset(['0'])),('chunks',frozenset(['2048','0']))])
 
     >>> dom = Dom([('listen', frozenset(['1', '0'])), ('timeout', frozenset(['1', '0'])), ('ssl', frozenset(['1', '0'])), ('local', frozenset(['1', '0'])), ('anon', frozenset(['1', '0'])), ('log', frozenset(['1', '0'])), ('chunks', frozenset(['0', '65536', '4096', '2048']))])
-
-    >>> c2.z3expr(dom.z3db)
-    Traceback (most recent call last):
-    ...
-    AssertionError: cannot create z3expr from 'None'
 
     >>> assert c1.z3expr(dom.z3db) is z3util.TRUE
     >>> print c3.z3expr(dom.z3db)
@@ -402,63 +426,54 @@ class Core(object):
         anon == 0,
         Or(chunks == 0, chunks == 2048))
     """
-    def __init__(self,core):
+    def __init__(self,c=HDict()):
+        HDict.__init__(self,c)
+        
         if CM.__vdebug__:
-            assert (core is None or
-                     (isinstance(core,HDict) and
-                      all(is_csetting(s) for s in core.iteritems())))
-    
-        self.core = core
+            assert all(is_csetting(s) for s in self.iteritems()), self
+        
     def __str__(self):
-        if self.core is None:
-            return str(self.core) #never reached
-        if not self.core:
+        if not self:
             return "true"  #no constraint
-        return ' '.join(map(str_of_csetting,self.core.iteritems()))
-
-    def __hash__(self): return hash(self.core)
-    def __eq__(self,o): return self.core == o.core
-    def __lt__(self,o): return self.core < o.core
+        return ' '.join(map(str_of_csetting,self.iteritems()))
 
     def z3expr(self,z3db):
-        assert self.core is not None, \
-            "cannot create z3expr from '{}'".format(self)
-
-        if not self.core:
+        if not self:
             return z3util.TRUE
         else:
             f = []
-            for vn,vs in self.core.iteritems():
+            for vn,vs in self.iteritems():
                 vn_,vs_ = z3db[vn]
                 f.append(z3util.myOr([vn_ == vs_[v] for v in vs]))
 
             return z3util.myAnd(f)        
-
+        
 
 class CDCore(tuple):
     """
     >>> dom = Dom([('listen', frozenset(['1', '0'])), ('timeout', frozenset(['1', '0'])), ('ssl', frozenset(['1', '0'])), ('local', frozenset(['1', '0'])), ('anon', frozenset(['1', '0'])), ('log', frozenset(['1', '0'])), ('chunks', frozenset(['0', '65536', '4096', '2048']))])
 
-    >>> c = CDCore((Core(None),Core(None)))
+    >>> c = CDCore.mk_default()
     >>> print c
     c(None), d(None)
 
     >>> assert c.z3expr(dom.z3db) is None
 
-    >>> c = CDCore((Core(HDict([('listen', frozenset(['1'])), ('timeout', frozenset(['1']))])), Core(None)))
+    >>> c = CDCore((Core([('listen', frozenset(['1'])), ('timeout', frozenset(['1']))]), None))
     >>> print c
     c(listen=1 timeout=1), d(None)
     
     >>> print c.z3expr(dom.z3db)
     And(listen == 1, timeout == 1)
 
-    >>> print CDCore((Core(HDict()),Core(None))).z3expr(dom.z3db)
+    >>> print CDCore((Core(),None)).z3expr(dom.z3db)
     True
 
     """    
     def __init__(self,(cc,dc)):
         if CM.__vdebug__:
-            assert isinstance(cc,Core) and isinstance(dc,Core), (cc,dc)
+            assert cc is None or isinstance(cc,Core), cc
+            assert dc is None or isinstance(dc,Core), dc
         
         tuple.__init__(self,(cc,dc))
 
@@ -475,55 +490,60 @@ class CDCore(tuple):
         conj = lambda: self.conj.z3expr(z3db)
         disj = lambda: z3.Not(self.disj.z3expr(z3db))
 
-        if self.conj.core is None: # no data
+        if self.conj is None: # no data
             return None
-        elif self.conj.core:
-            if self.disj.core:
+        elif self.conj:
+            if self.disj:
                 return z3.simplify(z3.And(conj(),disj()))
             else:
                 return conj()
         else:
-            if self.disj.core:
+            if self.disj:
                 return disj()
             else:
                 return z3util.TRUE
-            
+
+    @staticmethod
+    def mk_default():
+        return CDCore((None,None))
 
 class PNCore(tuple):
     """
     >>> dom = Dom([('listen', frozenset(['1', '0'])), ('timeout', frozenset(['1', '0'])), ('ssl', frozenset(['1', '0'])), ('local', frozenset(['1', '0'])), ('anon', frozenset(['1', '0'])), ('log', frozenset(['1', '0'])), ('chunks', frozenset(['0', '65536', '4096', '2048']))])
 
+    >>> print PNCore.mk_default()
+    p: c(None), d(None), n: c(None), d(None)
 
-    >>> pcc = HDict([('listen', frozenset(['1'])), ('timeout', frozenset(['1']))])
+    >>> pcc = Core([('listen', frozenset(['1'])), ('timeout', frozenset(['1']))])
     >>> pdc = None
-    >>> ncc = HDict()
-    >>> ndc = HDict([('listen', frozenset(['1'])), ('timeout', frozenset(['1']))])
+    >>> ncc = Core()
+    >>> ndc = Core([('listen', frozenset(['1'])), ('timeout', frozenset(['1']))])
 
-    >>> c = PNCore((CDCore((Core(pcc),Core(pdc))),CDCore((Core(ncc),Core(ndc)))))
+    >>> c = PNCore((CDCore((pcc,pdc)),CDCore((ncc,ndc))))
     >>> print c
     p: c(listen=1 timeout=1), d(None), n: c(true), d(listen=1 timeout=1)
 
     >>> print c.z3expr(dom.z3db)
     And(listen == 1, timeout == 1)
 
-    >>> assert c.strength == 2
+    >>> assert len(c.settings) == 2
 
 
-    >>> pcc = HDict([('listen', frozenset(['0']))])
+    >>> pcc = Core([('listen', frozenset(['0']))])
     >>> pdc = None
-    >>> ncc = HDict([('listen', frozenset(['1']))])
+    >>> ncc = Core([('listen', frozenset(['1']))])
     >>> ndc = None
-    >>> c = PNCore((CDCore((Core(pcc),Core(pdc))),CDCore((Core(ncc),Core(ndc)))))
+    >>> c = PNCore((CDCore((pcc,pdc)),CDCore((ncc,ndc))))
     >>> print c.z3expr(dom.z3db)
     listen == 0
 
-    >>> assert c.strength == 2
+    >>> assert len(c.settings) == 2
 
-    >>> c = PNCore((CDCore((Core(HDict()),Core(None))),CDCore((Core(None),Core(HDict())))))
+    >>> c = PNCore((CDCore((Core(),None)),CDCore((None,Core()))))
     >>> print c.z3expr(dom.z3db)
     True
 
-    >>> assert c.strength == 0
+    >>> assert len(c.settings) == 0
     """
     def __init__(self,(pc,nc)):
         if CM.__vdebug__:
@@ -539,39 +559,38 @@ class PNCore(tuple):
     def __str__(self):
         return "p: {}, n: {}".format(self.pcore,self.ncore)
         
+    # def repair(self,configs,dom):
+    #     """
+    #     Return a *real* inv/overapprox over configs
+    #     inv = (conj,disj)
+    #     """
+    #     #pos traces => p1 & not(n2)
+    #     if p1: #x & y 
+    #         if not all(c_implies(c,p1) for c in configs):
+    #             p1 = None
 
-    def repair(self,configs,dom):
-        """
-        Return a *real* inv/overapprox over configs
-        inv = (conj,disj)
-        """
-        #pos traces => p1 & not(n2)
-        if p1: #x & y 
-            if not all(c_implies(c,p1) for c in configs):
-                p1 = None
+    #     if n2: # y & z => not(y) | not(z)
+    #         n2 = neg_of_core(n2,core,dom)
+    #         if not all(d_implies(c,n2) for c in configs):
+    #             n2 = None
 
-        if n2: # y & z => not(y) | not(z)
-            n2 = neg_of_core(n2,core,dom)
-            if not all(d_implies(c,n2) for c in configs):
-                n2 = None
-
-        #neg traces => n1 & not(p2)
-        #not(n1) || p2
-        if n1 is None and p2 is None:
-            pass
-        elif n1 is not None and p2 is None:
-            n1 = neg_of_core(n1,core,dom)
-            if not all(d_implies(c,n1) for c in configs):
-                n1 = None
-        elif n1 is None and p2 is not None:
-            if not all(c_implies(c,p2) for c in configs):
-                p2 = None
-        else:
-            n1 = neg_of_core(n1,core,dom)
-            if not all(d_implies(c,n1) or c_implies(c,p2) 
-                       for c in configs):
-                n1 = None
-                p2 = None
+    #     #neg traces => n1 & not(p2)
+    #     #not(n1) || p2
+    #     if n1 is None and p2 is None:
+    #         pass
+    #     elif n1 is not None and p2 is None:
+    #         n1 = neg_of_core(n1,core,dom)
+    #         if not all(d_implies(c,n1) for c in configs):
+    #             n1 = None
+    #     elif n1 is None and p2 is not None:
+    #         if not all(c_implies(c,p2) for c in configs):
+    #             p2 = None
+    #     else:
+    #         n1 = neg_of_core(n1,core,dom)
+    #         if not all(d_implies(c,n1) or c_implies(c,p2) 
+    #                    for c in configs):
+    #             n1 = None
+    #             p2 = None
         
     def z3expr(self,z3db):
         z3_pc = self.pcore.z3expr(z3db)
@@ -599,64 +618,39 @@ class PNCore(tuple):
     def settings(self):
         cores = [self.pcore.conj,self.pcore.disj,
                  self.ncore.conj,self.ncore.disj]
-        cores = [c for c in cores if c.core]
+        cores = [c for c in cores if c]
         settings = []
         for c in cores:
-            settings.extend(c.core.hcontent)
+            settings.extend(c.hcontent)
         settings = set(settings)
         return settings
-    @property
-    def strength(self):
-        return len(self.settings)
-
-
-class Cores_d(OrderedDict):
-    def __init__(self,d):
-        OrderedDict.__init__(self,d)
-        if CM.__vdebug__:
-            assert (all(isinstance(sid,str) and isinstance(c,PNCore)
-                        for sid,c in self.iteritems())), d
-    def __str__(self):
-        return '\n'.join("{}. {}: {}"
-                         .format(i+1,sid,cores_d[sid])
-                         for i,sid in enumerate(sorted(self)))
 
     @staticmethod
-    def merge(self):
-        mcores_d = MCores_d()
-        for sid,pncore in cores_d.iteritems():
+    def mk_default():
+        return PNCore((CDCore.mk_default(),CDCore.mk_default()))
 
-            if pncore in mcores_d:
-                mcores_d[pncore].add(sid)
-            else:
-                mcores_d[pncore] = set([sid])
 
-        for sid in mcores_d:
-            mcores_d[sid]=frozenset(mcores_d[sid])
-
-        return mcores_d    
-    
-
-class MCores_d(OrderedDict):
-    def __init__(self,d):
+class CORES_D(OrderedDict):
+    def __init__(self,d=OrderedDict()):
         OrderedDict.__init__(self,d)
+
         if CM.__vdebug__:
-            assert all(isinstance(c,PNCore) and is_cov(cov)
-                       for c,cov in self.iteritems())
+            assert all(isinstance(sid,str) and isinstance(c,PNCore)
+                       for sid,c in self.iteritems())
 
     def __str__(self):
-        mcores_d_ = sorted(self.iteritems(),
-                           key=lambda (c,cov):(c.strength,len(cov)))
+        return '\n'.join("{}. {}: {}"
+                         .format(i+1,sid,self[sid])
+                         for i,sid in enumerate(sorted(self)))
+    
 
-        return '\n'.join("{}. ({}) {}: {}"
-                         .format(i+1,
-                                 pncore.strength,
-                                 pncore,
-                                 str_of_cov(cov))
-                         for i,(pncore,cov) in enumerate(mcores_d_))
+
 
 
 
 def doctestme():
     import doctest
     doctest.testmod()
+
+
+
