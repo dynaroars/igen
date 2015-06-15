3d8e91ebd237d148bc6233234afbe4977da79123
ThanhVu (Vu) Nguyen @ GiaoChi <20884260+nguyenthanhvuh@users.noreply.github.com>
2015-06-15 11:49:05 -0400

    rearrange testsuite,  add ts for cmd cat

--- src/config.py
+++ src/config.py
@@ -725,10 +725,6 @@ def igen(dom,get_cov,seed=None,cover_siz=None,config_default=None):
             break
         
         cur_iter += 1
-
-        # print str_of_cores_d(cores_d)
-        # print '\n'.join(map(str_of_pncore,set(cores_d.values())))
-        #gen new configs
         sel_core,configs = gen_configs_iter(set(cores_d.values()),
                                             ignore_sel_cores,
                                             configs_d,z3db,dom)
@@ -754,7 +750,7 @@ def igen(dom,get_cov,seed=None,cover_siz=None,config_default=None):
     logger.info(str_of_summary(seed,cur_iter,time()-st,cov_time,
                                len(configs_d),len(covs_d),
                                tmpdir))
-
+    logger.info("Done seed {} (test {})".format(seed,random.randrange(100)))
     return pp_cores_d,cores_d,configs_d,covs_d,dom
 
 #Shortcuts
@@ -1028,7 +1024,7 @@ def show_dtrace(dt):
 
 def str_of_summary(seed,iters,ntime,ctime,nconfigs,ncovs,tmpdir):
     s = ("Summary: " +
-         "Seed {} (test {}), ".format(seed,random.randrange(1000)) +
+         "Seed {}, ".format(seed) +
          "Iters {}, ".format(iters) +
          "Time ({}s, {}s), ".format(ntime,ctime) +
          "Configs {}, ".format(nconfigs) +
@@ -1284,9 +1280,9 @@ def void_run_single(cmd):
     
 def void_run(cmds):
     "just exec command, does not return anything"
-
-    if not isinstance(cmds,list):
-        cmds = [cmds]
+    assert cmds, cmds
+    
+    if not CM.is_iterable(cmds): cmds = [cmds]
     logger.detail('run {} cmds'.format(len(cmds)))
     for cmd in cmds:
         void_run_single(cmd)
@@ -1317,7 +1313,6 @@ def get_cov_wrapper(config,args):
         raise
 
         
-
 #Motivation/Simple examples
 def prepare_motiv(dom_file,prog_name):
     if CM.__vdebug__:
@@ -1390,324 +1385,6 @@ def get_cov_motiv_gcov(config,args):
             for f in os.listdir(gcov_dir) if f.endswith(".gcov"))
     sids = set(CM.iflatten(sids))
     return sids
-
-#Coreutils
-def prepare_coreutils(prog_name):
-    if CM.__vdebug__:
-        assert isinstance(prog_name,str),prog_name
-
-    main_dir = getpath('~/Dropbox/git/config/benchmarks/coreutils')
-    dom_file = os.path.join(main_dir,"doms","{}.dom".format(prog_name))
-    dom_file = getpath(dom_file)
-    assert os.path.isfile(dom_file),dom_file
-
-    dom,_ = Dom.get_dom(dom_file)
-    logger.info("dom_file '{}': {}".format(dom_file,dom))
-
-    bdir = os.path.join(main_dir,'coreutils')
-    prog_dir = os.path.join(bdir,'obj-gcov','src')
-    prog_exe = os.path.join(prog_dir,prog_name)
-    assert os.path.isfile(prog_exe),prog_exe
-    logger.info("prog_exe: '{}'".format(prog_exe))
-
-    dir_ = os.path.join(bdir,'src')
-    assert os.path.isdir(dir_)
-    args = {'var_names':dom.keys(),
-            'prog_name': prog_name,
-            'prog_exe': prog_exe,
-            'get_cov': get_cov_coreutils,
-            'testsuite_f': coreutils_d[prog_name],
-            'prog_dir':prog_dir,
-            'dir_': dir_,
-            'main_dir':main_dir}
-    get_cov = lambda config: get_cov_wrapper(config,args)    
-    return dom,get_cov
-
-def getopts_coreutils(config,ks):
-    """
-    >>> ks = "-x -y z + --o".split()
-    >>> config = dict(zip(ks,'on off something "%M" "date"'.split()))
-    >>> assert getopts_coreutils(config,ks) == '-x z something +"%M" --o="date"'
-    
-    """
-    opts = []
-    for k in ks:
-        if config[k] == "off":
-            continue
-        elif config[k] == "on":
-            opts.append(k)
-        elif k == "+": #nospace
-            opts.append("{}{}".format(k,config[k]))
-        elif k.startswith("--"):  # --option=value
-            opts.append("{}={}".format(k,config[k]))
-        else: #k v
-            opts.append("{} {}".format(k,config[k]))
-
-    return ' '.join(opts)
-
-def get_cov_coreutils(config,args):
-    """
-    >>> args = {'prog_dir': '/home/tnguyen/Dropbox/git/config/benchmarks/coreutils/coreutils/obj-gcov/src', 'var_names': ['-a', '-s', '-n', '-r', '-v', '-m', '-p', '-i', '-o', '--help', '--version'], 'dir_': '/home/tnguyen/Dropbox/git/config/benchmarks/coreutils/coreutils/src', 'prog_exe': '/home/tnguyen/Dropbox/git/config/benchmarks/coreutils/coreutils/obj-gcov/src/uname', 'prog_name': 'uname'}
-    >>> config = HDict([('-a', 'on'), ('-s', 'off'), ('-n', 'off'), ('-r', 'off'), ('-v', 'off'), ('-m', 'off'), ('-p', 'off'), ('-i', 'off'), ('-o', 'off'), ('--help', 'off'), ('--version', 'off')])
-
-    #>>> sids = get_cov_coreutils(config,args);  len(sids) == 
-
-    """
-    if CM.__vdebug__:
-        assert isinstance(args,dict), args
-        assert 'main_dir' in args
-        assert 'dir_' in args
-        assert 'prog_dir' in args
-        assert 'prog_name' in args
-        assert 'prog_exe' in args
-        assert 'testsuite_f' in args
-        assert 'var_names' in args
-
-
-    dir_ = args['dir_']
-    main_dir = args['main_dir']    
-    prog_dir = args['prog_dir']
-    prog_name = args['prog_name']
-    prog_exe = args['prog_exe']
-    testsuite_f = args['testsuite_f']
-    var_names = args['var_names']
-    opts = getopts_coreutils(config,var_names)
-
-    #cleanup
-    cmd = "rm -rf {}/*.gcov {}/*.gcda".format(dir_,prog_dir)
-    void_run(cmd)
-    #CM.pause()
-    
-    #run testsuite
-    testfiles_dir = os.path.join(main_dir,'testfiles',prog_name)
-    margs = {'prog_exe':prog_exe, 'opts':opts,
-                 'testfiles_dir':testfiles_dir}
-    testsuite_f(margs)
-
-    #read traces from gcov
-    #/path/prog.Linux.exe -> prog
-    src_dir = os.path.join(dir_,'src')
-    cmd = "gcov {} -o {}".format(prog_name,prog_dir)
-    void_run(cmd)
-    
-    gcov_dir = os.getcwd()
-    sids = (parse_gcov(os.path.join(gcov_dir,f))
-            for f in os.listdir(gcov_dir) if f.endswith(".gcov"))
-    sids = set(CM.iflatten(sids))
-    if not sids:
-        logger.warn("config {} has NO cov".format(config))
-        
-    return sids
-
-def testsuite_default(args):
-    """
-    Simple run, e.g.,  uname -opts
-    """
-    if CM.__vdebug__:
-        assert isinstance(args,dict),args
-        assert 'opts' in args
-        assert 'prog_exe' in args
-
-    prog_exe = args['prog_exe']
-    opts = args['opts']
-    cmd = "{} {}".format(prog_exe,opts)
-    void_run(cmd)
-    
-    
-def testsuite_date(args):
-    if CM.__vdebug__:
-        assert isinstance(args,dict),args
-        assert 'opts' in args
-        assert 'prog_exe' in args
-        assert 'testfiles_dir' in args        
-
-    prog_exe = args['prog_exe']
-    opts = args['opts']
-
-    testfiles_dir = args['testfiles_dir']
-    date_file = os.path.join(testfiles_dir,'datefile')
-    assert os.path.isfile(date_file),date_file
-    
-    cmds = []
-    cmds.append("{} {}".format(prog_exe,opts))  #date -options
-    cmds.append("{} {} -d '2004-02-29 16:21:42'".format(prog_exe,opts))
-    cmds.append("{} {} -d 'next  Thursday'".format(prog_exe,opts))
-    cmds.append("{} {} -d 'Sun, 29 Feb 2004 16:21:42 -0800'"
-                .format(prog_exe,opts))
-    cmds.append("{} {} -d '@2147483647'".format(prog_exe,opts))
-    cmds.append("{} {} -d 'TZ=\"America/Los_Angeles\" 09:00 next Fri'"
-                .format(prog_exe,opts))
-    cmds.append("TZ='America/Los_Angeles' {} {}".format(prog_exe,opts))
-
-    cmds.append("{} {} -r /bin/date".format(prog_exe,opts)) #date -otions /bin/date
-    cmds.append("{} {} -r ~".format(prog_exe,opts)) #date -otions /bin/date
-
-    cmds.append("{} {} -f {}".format(prog_exe,opts,date_file)) #date -f filename
-    cmds.append("{} {} -f notexist".format(prog_exe,opts)) #date -f filename    
-    void_run(cmds)
-
-def testsuite_ls(args):
-    """
-    testsuite_ls({'opts':'-d','prog_exe':'/bin/ls', 'testfiles_dir':'/home/tnguyen/Dropbox/git/config/benchmarks/coreutils/testfiles/ls'})
-    """
-    if CM.__vdebug__:
-        assert isinstance(args,dict),args
-        assert 'opts' in args
-        assert 'prog_exe' in args
-        assert 'testfiles_dir' in args        
-
-    prog_exe = args['prog_exe']
-    opts = args['opts']
-    testfiles_dir = args['testfiles_dir']
-    tdir = os.path.join(args['testfiles_dir'],"edir")
-    cmds = []
-    cmds.append("{} {} {}/a_ln".format(prog_exe,opts,tdir))
-    cmds.append("{} {} {}/noexist_ln".format(prog_exe,opts,tdir))
-    cmds.append("{} {} {}/d1/.hidden1".format(prog_exe,opts,tdir))
-    cmds.append("{} {} {}/d1/d1d1/b".format(prog_exe,opts,tdir))
-    cmds.append("{} {} {}/d2/a.sh".format(prog_exe,opts,tdir))
-    cmds.append("{} {} {}/d2/dirA".format(prog_exe,opts,tdir))
-    cmds.append("{} {} {}/d2/dirA/*".format(prog_exe,opts,tdir))
-    
-    cmds.append("{} {} {}/d2_ln/a.sh".format(prog_exe,opts,tdir))
-    cmds.append("{} {} {}/d2_ln/.hidden_dir".format(prog_exe,opts,tdir))
-
-    cmds.append("{} {} /boot".format(prog_exe,opts))
-    cmds.append("{} {} /boot/*".format(prog_exe,opts))
-    cmds.append("{} {} /bin/ls".format(prog_exe,opts))
-    assert os.path.isdir(getpath("~/ls_test")), "create ~/ls_test"
-    cmds.append("{} {} ~/ls_test".format(prog_exe,opts))
-    cmds.append("{} {} .".format(prog_exe,opts))
-    cmds.append("{} {} ..".format(prog_exe,opts))
-    void_run(cmds)
-
-def testsuite_md5sum(args):
-    """
-    """
-    if CM.__vdebug__:
-        assert isinstance(args,dict),args
-        assert 'opts' in args
-        assert 'prog_exe' in args
-        assert 'testfiles_dir' in args        
-
-    prog_exe = args['prog_exe']
-    opts = args['opts']
-    tdir = args['testfiles_dir']
-
-    cmds = []
-    cmds.append("{} {} {}/file1".format(prog_exe,opts,tdir))
-    cmds.append("{} {} {}/file2".format(prog_exe,opts,tdir))
-    cmds.append("{} {} {}/binary.dat".format(prog_exe,opts,tdir))    
-    cmds.append("{} {} {}/file1 {}/binary.dat".format(prog_exe,opts,tdir,tdir))
-    cmds.append("{} {} {}/*".format(prog_exe,opts,tdir))
-    cmds.append("cat {}/file1 | {} {} -".format(tdir,prog_exe,opts))    
-    cmds.append("{} {} {}/results.md5".format(prog_exe,opts,tdir))
-    cmds.append("{} {} {}/results_failed.md5".format(prog_exe,opts,tdir))
-    
-    void_run(cmds)
-    
-def testsuite_sort(args):
-    """
-    Examples for tests: http://www.theunixschool.com/2012/08/linux-sort-command-examples.html
-    """
-    if CM.__vdebug__:
-        assert isinstance(args,dict),args
-        assert 'opts' in args
-        assert 'prog_exe' in args
-        assert 'testfiles_dir' in args        
-
-    prog_exe = args['prog_exe']
-    opts = args['opts']
-    tdir = args['testfiles_dir']
-
-    cmds = []
-    #cmds.append("wc -l /usr/bin/* | {} {}".format(prog_exe,opts)) #wc -l . | sort
-    cmds.append("{} {} {}/file1".format(prog_exe,opts,tdir))  #sort file1
-    cmds.append("{} {} {}/file3".format(prog_exe,opts,tdir))  #sort file1
-    cmds.append("{} {} {}/file1 {}/file2 {}/file3"  
-                .format(prog_exe,opts,tdir,tdir,tdir)) #multiple fiels
-
-    #some error with this command, cannot read
-    # cmds.append("{} {} --files0-from={}/allfiles"
-    #             .format(prog_exe,opts,tdir)) #multiple fiels
-    
-    void_run(cmds)
-
-def testsuite_pr(args):
-    """
-    ls -a | pr -n -h "Files in $(pwd)" > directory.txt
-    pr -l 40 -h Using Hypertext comms_paper
-    pr -n2
-    http://docstore.mik.ua/orelly/unix3/upt/ch21_15.htm
-    """
-    pass
-
-def testsuite_id(args):
-    if CM.__vdebug__:
-        assert isinstance(args,dict),args
-        assert 'opts' in args
-        assert 'prog_exe' in args
-        assert 'testfiles_dir' in args        
-
-    prog_exe = args['prog_exe']
-    opts = args['opts']
-
-    cmds = []
-    cmds.append("{} {}".format(prog_exe,opts))
-    void_run(cmds)
-
-    
-def testsuite_ln(args):
-    if CM.__vdebug__:
-        assert isinstance(args,dict),args
-        assert 'opts' in args
-        assert 'prog_exe' in args
-
-        assert 'testfiles_dir' in args        
-
-
-    prog_exe = args['prog_exe']
-    opts = args['opts']
-    testfiles_dir = args['testfiles_dir']
-
-    cmds = []
- #test 1
-    cmds = []    
-    cmds.append("touch a")
-    cmds.append("ln {} a b".format(opts_str))
-
-    #test 2
-    cmds = []        
-    cmds.append("mkdir d")
-    cmds.append("ln {} d e".format(opts_str))
-    myexec(test_dir,cmds)
-    
-    #from coreutils tests
-    #test 3  # d/f -> ../f
-    cmds = []        
-    cmds.append("mkdir d")
-    cmds.append("ln {} --target_dir=d ../f".format(opts_str))
-    myexec(test_dir,cmds)
-    
-    #test 4
-    # Check that a target directory of '.' is supported
-    # and that indirectly specifying the same target and link name
-    # through that is detected.
-    cmds = []        
-    cmds.append("ln {} . b".format(opts_str))
-    cmd.append("echo foo > a")      
-    cmds.append("ln {} a b > err 2>&1)".format(opts_str))
-    myexec(test_dir,cmds)
-
-
-coreutils_d = {'date': testsuite_date,
-               'ls': testsuite_ls,
-               'ln': testsuite_ln,
-               'sort': testsuite_sort,
-               'id': testsuite_id,
-               'md5sum': testsuite_md5sum,
-               'uname': testsuite_default}
-        
 
 
 def doctestme():
