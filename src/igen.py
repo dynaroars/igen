722987b4a2651b69b038111b321ac7ab82be2661
ThanhVu (Vu) Nguyen @ GiaoChi <nguyenthanhvuh@gmail.com>
2015-06-17 14:06:37 -0400

    added benchmarks

--- src/igen.py
+++ src/igen.py
@@ -1,3 +1,7 @@
+import os.path
+import tempfile
+from time import time
+
 import vu_common as CM
 import config
 import config_coreutils
@@ -11,6 +15,36 @@ examples_d = {"ex_motiv1": "ex_motiv1",
               "ex_motiv2a" : "ex_motiv2",              
               "ex_motiv2b" : "ex_motiv2",
               "ex_motiv2c" : "ex_motiv2"}
+
+def get_run_f(args):
+    if args.prog in otter_d:
+        dom,get_cov,pathconds_d=config.prepare_otter(args.prog)
+        igen = config.IGen(dom,get_cov,config_default=None)
+        if args.do_gt or args.do_full:
+            if args.rand_n:
+                _f = lambda _,tdir: config.do_gt(dom,pathconds_d,n=args.rand_n,tmpdir=tdir)
+            else:
+                _f = lambda _,tdir: config.do_gt(dom,pathconds_d,tmpdir=tdir)
+        elif args.rand_n is None:
+            _f = lambda seed,tdir: igen.go(seed=seed)
+        else:
+            _f = lambda seed,tdir: igen.go_rand(rand_n=args.rand_n,seed=seed,tmpdir=tdir)
+    else:
+        if args.prog in examples_d:
+            dom,get_cov=config.prepare_motiv(examples_d[args.prog],args.prog)
+        elif args.prog in config_coreutils.coreutils_d:
+            dom,get_cov=config_coreutils.prepare(args.prog)            
+        else:
+            raise AssertionError("unrecognized prog '{}'".format(args.prog))
+
+        igen = config.IGen(dom,get_cov,config_default=None)
+        if args.do_full:
+            _f = lambda tdir: igen.go_full(tmpdir=tdir)
+        elif args.rand_n is None:
+            _f = lambda seed,tdir: igen.go(seed=seed,tmpdir=tdir)
+        else:
+            _f = lambda seed,tdir: igen.go_rand(rand_n=args.rand_n,seed=seed,tmpdir=tdir)
+        return _f
 
 if __name__ == "__main__":
     """
@@ -36,57 +70,71 @@ if __name__ == "__main__":
     aparser.add_argument("--replay",
                          help="replay info from run dir",
                          action="store_true")
+
+    aparser.add_argument("--replay_dirs",
+                         help="replay info from adir containing multiple run dirs",
+                         action="store_true")
     
     aparser.add_argument("--seed",
                          type=int,
                          help="use this seed")
 
-    aparser.add_argument("--n",
+    aparser.add_argument("--rand_n",
                          type=int,
-                         help="n is an integer")
+                         help="rand_n is an integer")
 
     aparser.add_argument("--do_full",
                          help="use all possible configs",
                          action="store_true")
 
-    #Options for running otter
     aparser.add_argument("--do_gt",
                          help="obtain ground truths",
                          action="store_true")
 
+    aparser.add_argument("--show_cov",
+                         help="show coverage info",
+                         action="store_true")
+    
+    aparser.add_argument("--do_mixed_conj_disj",
+                         help="do both conj and disj interactions",
+                         action="store_true")
+
+    aparser.add_argument("--benchmark",
+                         type=int,
+                         help="do benchmark")
+    
     args = aparser.parse_args()
     prog = args.prog
     config.logger.level = args.logger_level
     CM.__vdebug__ = args.debug
-    seed = args.seed
 
     if args.replay:
-        config.replay(prog)
+        config.Analysis.replay(prog)
+        exit(0)
+        
+    if args.replay_dirs:
+        config.Analysis.replay_dirs(prog)
+        exit(0)
 
-    elif prog in otter_d:
-        dom,get_cov,pathconds_d=config.prepare_otter(prog)
-        if args.do_gt or args.do_full:
-            if args.n:
-                _ = config.do_gt(dom,pathconds_d,n=args.n)
-            else:
-                _ = config.do_gt(dom,pathconds_d)
-        elif args.n:
-            _ = _ = config.igen_rand(dom,get_cov,rand_n=args.n,seed=seed)
-        else:
-            config.igen(dom,get_cov,seed=seed)
-            
+
+    nruns = args.benchmark if args.benchmark else 1
+    _f = get_run_f(args)        
+    tdir = tempfile.mkdtemp(dir='/var/tmp',prefix="vu_bm")
+    st = time()
+    if args.seed is None:
+        seed = round(time(),2)
     else:
-        if prog in examples_d:
-            dom,get_cov=config.prepare_motiv(examples_d[prog],prog)
-        elif prog in config_coreutils.coreutils_d:
-            dom,get_cov=config_coreutils.prepare(prog)            
-        else:
-            raise AssertionError("unrecognized prog '{}'".format(prog))
+        seed = float(args.seed)
 
-        if args.do_full:
-            _ = config.igen_full(dom,get_cov)
-        elif args.n:
-            _ = config.igen_rand(dom,get_cov,rand_n=args.n,seed=seed)
-        else:
-            _ = config.igen(dom,get_cov,seed=seed)
+    for i in range(nruns):        
+        st_ = time()
+        seed_ = seed + i
+        tdir_ = tempfile.mkdtemp(dir=tdir,prefix="run{}_".format(i))
+        print("*run {}/{}".format(i+1,nruns))
+        _ = _f(seed_,tdir_)
+        print("*run {}, seed {}, time {}s, '{}'".format(i+1,seed_,time()-st_,tdir_))
+
+    print("** done with {} runs, seed {}, time {}s, results stored in '{}'"
+          .format(nruns,seed,time()-st,tdir))
+        
 
