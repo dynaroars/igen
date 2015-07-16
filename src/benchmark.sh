8f94dcde52f5703cfdfb3ec95e64d9d4d0c50c30
ThanhVu (Vu) Nguyen @ GiaoChi <20884260+nguyenthanhvuh@users.noreply.github.com>
2015-07-16 01:02:44 -0400

    change do_gt to do_full in benchmark.sh

--- src/benchmark.sh
+++ src/benchmark.sh
@@ -1,12 +1,12 @@
 #Otter
 prog="vsftpd"
 echo ${prog}
-python igen.py ${prog}  --logger_level 4 --seed 0 --do_gt > ~/Dropbox1/config_results/${prog}_full
+python igen.py ${prog}  --logger_level 4 --seed 0 --do_full > ~/Dropbox1/config_results/${prog}_full
 python igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 10 > ~/Dropbox1/config_results/${prog}_bm10
 
 prog="ngircd"
 echo ${prog}
-python igen.py ${prog}  --logger_level 4 --seed 0 --do_gt > ~/Dropbox1/config_results/${prog}_full
+python igen.py ${prog}  --logger_level 4 --seed 0 --do_full > ~/Dropbox1/config_results/${prog}_full
 python igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 10 > ~/Dropbox1/config_results/${prog}_bm10
 
 #CoreUtils
