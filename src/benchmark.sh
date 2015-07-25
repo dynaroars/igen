e4e6071dfee459102b4a96d597c066c248d52ec4
ThanhVu Nguyen <nguyenthanhvuh@gmail.com>
2015-07-25 18:21:04 -0400

    add benchmark_full

--- src/benchmark.sh
+++ src/benchmark.sh
@@ -1,38 +1,31 @@
 #Otter
 prog="vsftpd"
 echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --do_full > ~/Dropbox1/config_results/${prog}_full 2>&1
 python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
 
 prog="ngircd"
 echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --do_full > ~/Dropbox1/config_results/${prog}_full 2>&1
 python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
 
 #CoreUtils
 prog="id"
 echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --do_full > ~/Dropbox1/config_results/${prog}_full 2>&1
 python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
 
 prog="uname"
 echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --do_full > ~/Dropbox1/config_results/${prog}_full 2>&1
 python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
 
 prog="mv"
 echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --do_full > ~/Dropbox1/config_results/${prog}_full 2>&1
 python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
 
 prog="cat"
 echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --do_full > ~/Dropbox1/config_results/${prog}_full 2>&1
 python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
 
 prog="ln"
 echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --do_full > ~/Dropbox1/config_results/${prog}_full 2>&1
 python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
 
 prog="date"
