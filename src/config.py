766fdf86f1050e07c722891a28bda91cbee60fe1
ThanhVu (Vu) Nguyen @ GiaoChi <20884260+nguyenthanhvuh@users.noreply.github.com>
2015-09-03 10:15:05 -0400

    modification to neg core

--- src/config.py
+++ src/config.py
@@ -74,6 +74,7 @@ def str_of_setting((k,v)):
     """
     if CM.__vdebug__:
         assert is_setting((k,v)), (k,v)
+        
     return '{}={}'.format(k,v)
 
 is_csetting = lambda (k,vs): isinstance(k,str) and is_valset(vs)
@@ -92,36 +93,38 @@ def str_of_csetting((k,vs)):
 
 class Dom(OrderedDict):
     """
-    >>> dom0 = Dom([('x',frozenset(['1','2'])),('y',frozenset(['1']))])
-    >>> print dom0
-    2 vars and 2 pos configs
-    1. x: (2) 1,2
-    2. y: (1) 1
-
-    
-    >>> dom1 = Dom([('x',frozenset(['1','2'])),\
+    >>> dom = Dom([('x',frozenset(['1','2'])),\
     ('y',frozenset(['1'])),\
     ('z',frozenset(['0','1','2'])),\
     ('w',frozenset(['a','b','c']))\
     ])
 
-    >>> print dom1
+    >>> print dom
     4 vars and 18 pos configs
     1. x: (2) 1,2
     2. y: (1) 1
     3. z: (3) 0,1,2
     4. w: (3) a,b,c
 
-    >>> assert dom1.siz == len(dom1.gen_configs_full())
+    >>> assert dom.siz == len(dom.gen_configs_full()) == 18
 
     >>> random.seed(0)
-    >>> configs = dom1.gen_configs_rand(5)
+    >>> configs = dom.gen_configs_rand(5)
     >>> print "\\n".join(map(str,configs))
     x=2 y=1 z=2 w=a
     x=2 y=1 z=2 w=b
     x=2 y=1 z=0 w=a
     x=1 y=1 z=0 w=a
     x=1 y=1 z=2 w=c
+
+    >>> configs = dom.gen_configs_tcover1()
+    >>> print "\\n".join(map(str,configs))
+    x=1 y=1 z=2 w=b
+    x=2 y=1 z=1 w=c
+    x=2 y=1 z=0 w=a
+
+    >>> assert len(dom.z3db) == len(dom) and set(dom.z3db) == set(dom)
+
     """
     def __init__(self,dom):
         OrderedDict.__init__(self,dom)
@@ -216,9 +219,20 @@ class Dom(OrderedDict):
     
 class Config(HDict):
     """
-    >>> c1 = Config([('a', '0'), ('b', '0'), ('c', '0')])
-    >>> print c1
-    a=0 b=0 c=0
+    >>> c = Config([('a', '1'), ('b', '0'), ('c', '1')])
+    >>> print c
+    a=1 b=0 c=1
+
+    >>> assert c.c_implies(Core()) and c.d_implies(Core())
+    
+    >>> core1 = Core([('a', frozenset(['0','1'])), ('b', frozenset(['0','1']))])    
+    >>> assert c.c_implies(core1)
+    >>> assert c.d_implies(core1)
+
+    >>> core2 = Core([('a', frozenset(['0','1'])), ('x', frozenset(['0','1']))])    
+    >>> assert not c.c_implies(core2)
+    >>> assert c.d_implies(core2)
+
     """
     def __init__(self,config=HDict()):
         HDict.__init__(self,config)
@@ -237,16 +251,20 @@ class Config(HDict):
 
     def c_implies(self,core):
         """
+        self => conj core
         x=0&y=1 => x=0,y=1
         not(x=0&z=1 => x=0,y=1)
         """
         if CM.__vdebug__:
-            assert isinstance(core,Core),core
+            assert isinstance(core,Core),(core)
 
         return (not core or
                 all(k in self and self[k] in core[k] for k in core))
 
-    def d_implies(self,core): 
+    def d_implies(self,core):
+        """
+        self => disj core
+        """
         if CM.__vdebug__:
             assert isinstance(core,Core),core
 
@@ -269,9 +287,18 @@ class Core(HDict):
     >>> print Core()
     true
 
-    >>> c = Core([('a',frozenset(['2'])),('c',frozenset(['0','1']))])
+    >>> c = Core([('x',frozenset(['2'])),('y',frozenset(['1'])),('z',frozenset(['0','1']))])
     >>> print c
-    a=2 c=0,1
+    x=2 y=1 z=0,1
+
+    >>> dom = Dom([('x',frozenset(['1','2'])),\
+    ('y',frozenset(['1'])),\
+    ('z',frozenset(['0','1','2'])),\
+    ('w',frozenset(['a','b','c']))\
+    ])
+
+    >>> print c.neg(dom)
+    x=1 z=2
     """
     def __init__(self,core=HDict()):
         HDict.__init__(self,core)
@@ -291,7 +318,8 @@ class Core(HDict):
         except AttributeError:
             if CM.__vdebug__:
                 assert isinstance(dom,Dom),dom
-            self._neg = Core((k,dom[k]-self[k]) for k in self)
+            ncore = ((k,dom[k]-self[k]) for k in self)
+            self._neg = Core([(k,vs) for k,vs in ncore if vs])
             return self._neg
         
     @staticmethod
@@ -1322,243 +1350,6 @@ class DTrace(object):
         logger.debug("infer {} interactions".format(len(mcores_d)))
         logger.detail('\n{}'.format(mcores_d))
         logger.info("strens: {}".format(mcores_d.strens_str))
-
-# class Analysis(object):
-#     def __init__(self,tmpdir):
-#         self.tmpdir = tmpdir
-
-#     def save_pre(self,seed,dom):
-#         CM.vsave(os.path.join(self.tmpdir,'pre'),(seed,dom))
-#     def save_post(self,pp_cores_d,itime_total):
-#         CM.vsave(os.path.join(self.tmpdir,'post'),(pp_cores_d,itime_total))
-#     def save_iter(self,cur_iter,dtrace):
-#         CM.vsave(os.path.join(self.tmpdir,'{}.tvn'.format(cur_iter)),dtrace)
-
-#     @staticmethod
-#     def load_pre(dir_):
-#         seed,dom = CM.vload(os.path.join(dir_,'pre'))
-#         return seed,dom
-#     @staticmethod
-#     def load_post(dir_):
-#         pp_cores_d,itime_total = CM.vload(os.path.join(dir_,'post'))
-#         return pp_cores_d,itime_total
-#     @staticmethod
-#     def load_iter(dir_,f):
-#         dtrace =CM.vload(os.path.join(dir_,f))
-#         return dtrace
-    
-#     @staticmethod
-#     def str_of_summary(seed,iters,itime,xtime,nconfigs,ncovs,tmpdir):
-#         ss = ["Seed {}".format(seed),
-#               "Iters {}".format(iters),
-#               "Time ({}s, {}s)".format(itime,xtime),
-#               "Configs {}".format(nconfigs),
-#               "Covs {}".format(ncovs),
-#               "Tmpdir {}".format(tmpdir)]
-#         return "Summary: " + ', '.join(ss)
-    
-#     @staticmethod
-#     def replay(dir_):
-#         """
-#         Replay execution info from saved info in dir_
-#         """
-
-#         def load_dir(dir_):
-#             seed,dom = Analysis.load_pre(dir_)
-#             dts = [Analysis.load_iter(dir_,f)
-#                    for f in os.listdir(dir_) if f.endswith('.tvn')]
-#             try:
-#                 pp_cores_d,itime_total = Analysis.load_post(dir_)
-#             except IOError:
-#                 logger.error("post info not avail")
-#                 pp_cores_d,itime_total = None,None
-#             return seed,dom,dts,pp_cores_d,itime_total
-
-#         seed,dom,dts,pp_cores_d,itime_total = load_dir(dir_)
-
-#         #print info
-#         logger.info("replay dir: '{}'".format(dir_))
-#         logger.info('seed: {}'.format(seed))
-#         logger.debug(dom.__str__())
-        
-#         #print iterations
-#         for dt in sorted(dts,key=lambda dt: dt.citer):
-#             dt.show()
-
-#         #print postprocess results
-#         mcores_d = pp_cores_d.merge()
-#         mcores_d.show_analysis()
-        
-#         #print summary
-#         xtime_total = itime_total - sum(dt.xtime for dt in dts)
-#         logger.info(Analysis.str_of_summary(
-#             seed,len(dts),itime_total,xtime_total,dt.nconfigs,dt.ncovs,dir_))
-
-#         return (len(dts),len(mcores_d),
-#                 itime_total,xtime_total,dt.nconfigs,dt.ncovs,
-#                 mcores_d.strens,mcores_d.vtyps)
-
-#     @staticmethod
-#     def replay_dirs(dir_):
-#         dir_ = getpath(dir_)
-#         logger.info("replay_dirs '{}'".format(dir_))
-        
-#         niters_total = 0
-#         nresults_total = 0        
-#         nitime_total = 0
-#         nxtime_total = 0    
-#         nconfigs_total = 0
-#         ncovs_total = 0
-#         strens_s = []
-#         ntyps_s = []
-
-#         #modified by ugur
-#         niters_arr = []
-#         nresults_arr = []
-#         nitime_arr = []
-#         nxtime_arr = [] 
-#         nconfigs_arr = []
-#         ncovs_arr = []
-#         counter = 0
-#         csv_arr = []
-
-#         for rdir in sorted(os.listdir(dir_)):
-#             rdir = os.path.join(dir_,rdir)
-#             (niters,nresults,itime,xtime,nconfigs,ncovs,strens,ntyps) = Analysis.replay(rdir)
-#             niters_total += niters
-#             nresults_total += nresults
-#             nitime_total += itime
-#             nxtime_total += xtime
-#             nconfigs_total += nconfigs
-#             ncovs_total += ncovs
-#             strens_s.append(strens)
-#             ntyps_s.append(ntyps)
-
-#             niters_arr.append(niters)
-#             nresults_arr.append(nresults)
-#             nitime_arr.append(itime)
-#             nxtime_arr.append(xtime)
-#             nconfigs_arr.append(nconfigs)
-#             ncovs_arr.append(ncovs)
-#             csv_arr.append("{},{},{},{},{},{},{},{},{}".format(counter,niters,nresults,itime,xtime,nconfigs,ncovs,','.join(map(str, ntyps)),','.join(map(str, strens))))
-#             counter += 1
-
-#         nruns_total = float(len(strens_s))
-
-#         ss = ["iter {}".format(niters_total/nruns_total),
-#               "results {}".format(nresults_total/nruns_total),
-#               "time {}".format(nitime_total/nruns_total),
-#               "xtime {}".format(nxtime_total/nruns_total),
-#               "configs {}".format(nconfigs_total/nruns_total),
-#               "covs {}".format(ncovs_total/nruns_total)]
-#         logger.info("STATS of {} runs (averages): {}".format(nruns_total,', '.join(ss)))
-        
-#         ssMed = ["iter {}".format(numpy.median(niters_arr)),
-#               "results {}".format(numpy.median(nresults_arr)),
-#               "time {}".format(numpy.median(nitime_arr)),
-#               "xtime {}".format(numpy.median(nxtime_arr)),
-#               "configs {}".format(numpy.median(nconfigs_arr)),
-#               "covs {}".format(numpy.median(ncovs_arr))]
-#         logger.info("STATS of {} runs (medians) : {}".format(nruns_total,', '.join(ssMed)))
-        
-#         ssSIQR = ["iter {}".format(Analysis.siqr(niters_arr)),
-#               "results {}".format(Analysis.siqr(nresults_arr)),
-#               "time {}".format(Analysis.siqr(nitime_arr)),
-#               "xtime {}".format(Analysis.siqr(nxtime_arr)),
-#               "configs {}".format(Analysis.siqr(nconfigs_arr)),
-#               "covs {}".format(Analysis.siqr(ncovs_arr))]
-#         logger.info("STATS of {} runs (SIQR)   : {}".format(nruns_total,', '.join(ssSIQR)))
-
-#         sres = {}
-#         for i,strens in enumerate(strens_s):
-#             logger.debug("run {}: {}".format(i+1,Mcores_d.str_of_strens(strens)))
-#             for strength,ninters,ncov in strens:
-#                 if strength not in sres:
-#                     sres[strength] = ([ninters],[ncov])
-#                 else:
-#                     inters,covs = sres[strength]
-#                     inters.append(ninters)
-#                     covs.append(ncov)
-
-#         ss = []
-#         medians = []
-#         siqrs = []
-#         tmp = []
-#         tex_table4=[]
-#         tex_table5=[]
-#         for strength in sorted(sres):
-#             inters,covs = sres[strength]
-#             length=len(inters)
-#             for num in range(length,int(nruns_total)):
-#                 inters.append(0)
-#                 covs.append(0)
-#             ss.append("({}, {}, {})".format(strength,sum(inters)/nruns_total,sum(covs)/nruns_total))
-#             medians.append("({}, {}, {})".format(strength, numpy.median(inters), numpy.median(covs)))
-#             siqrs.append("({}, {}, {})".format(strength, Analysis.siqr(inters), Analysis.siqr(covs)))
-#             tmp.append("{},{})".format(strength,','.join(map(str, inters))))
-#             tex_table4.append("{} \\mso{{{}}}{{{}}}".format(strength,numpy.median(inters),Analysis.siqr(inters)))
-#             tex_table5.append("{} \\mso{{{}}}{{{}}}".format(strength,numpy.median(covs),Analysis.siqr(covs)))
-        
-#         logger.info("interaction strens averages: {}".format(', '.join(ss)))
-#         logger.info("interaction strens medians : {}".format(', '.join(medians)))
-#         logger.info("interaction strens SIQRs   : {}".format(', '.join(siqrs)))
-#         #logger.info("interactions arrays   : {}".format('\n'.join(tmp)))
-        
-#         conjs = [c for c,_,_ in ntyps_s]
-#         disjs = [d for _,d,_ in ntyps_s]
-#         mixs = [m for _,_,m in ntyps_s]
-        
-#         length=len(conjs)
-#         for num in range(length,int(nruns_total)):
-#             conjs.append(0)
-        
-#         length=len(disjs)
-#         for num in range(length,int(nruns_total)):
-#             disjs.append(0)
-        
-#         length=len(mixs)
-#         for num in range(length,int(nruns_total)):
-#             mixs.append(0)
-        
-#         #logger.info("conjs array: {}".format(', '.join(map(str, conjs))))
-#         #logger.info("disjs array: {}".format(', '.join(map(str, disjs))))
-#         #logger.info("mixs  array: {}".format(', '.join(map(str, mixs))))
-
-#         nconjs = sum(conjs)/nruns_total
-#         ndisjs = sum(disjs)/nruns_total
-#         nmixs  = sum(mixs)/nruns_total
-        
-#         logger.info("interaction typs (averages): conjs {}, disjs {}, mixeds {}"
-#                     .format(nconjs,ndisjs,nmixs))            
-        
-#         logger.info("interaction typs (medians) : conjs {}, disjs {}, mixeds {}"
-#                     .format(numpy.median(conjs),numpy.median(disjs),numpy.median(mixs)))
-        
-#         logger.info("interaction typs (SIQRs)   : conjs {}, disjs {}, mixeds {}"
-#                     .format(Analysis.siqr(conjs),Analysis.siqr(disjs),Analysis.siqr(mixs)))
-
-#         logger.info("tex_table4:{}".format(' & '.join(tex_table4)))
-#         logger.info("tex_table5:{}".format(' & '.join(tex_table5)))
-
-#         logger.info("CVSs\n{}".format('\n'.join(csv_arr)))
-#         #end of modification
-
-#     @staticmethod
-#     def siqr(arr):
-#         return (numpy.percentile(arr, 75, interpolation='higher') - 
-#                 numpy.percentile(arr, 25, interpolation='lower'))/2
-    
-#     @staticmethod
-#     def debug_find_configs(sid,configs_d,find_in):
-#         if find_in:
-#             cconfigs_d = dict((c,cov) for c,cov in configs_d.iteritems()
-#                            if sid in cov)
-#         else:
-#             cconfigs_d = dict((c,cov) for c,cov in configs_d.iteritems()
-#                               if sid not in cov)
-
-#         logger.info(cconfigs_d)
-
 
 if __name__ == "__main__":
     import doctest
