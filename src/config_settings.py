3444a518e72886ecfccf8a9a853ee41c2109f05e
ThanhVu (Vu) Nguyen @ GiaoChi <20884260+nguyenthanhvuh@users.noreply.github.com>
2015-09-15 10:02:18 -0400

    reorganize get_cov examples

---
+++ src/config_settings.py
@@ -0,0 +1,13 @@
+
+#EXAMPLES
+
+#db is dictionary that has the name of the program exec and its domain file,
+#e.g., in examples_db, "ex":"ex" indicates the program name is ex.Linux.exe
+#(if runs on Linux) and the domain file is "ex.dom"
+
+example_dir = "../examples"
+example_db = {"ex": "ex"}
+
+
+
+
