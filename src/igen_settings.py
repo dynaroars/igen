a7236c37cae40ca9f2391f40a9fe057ed853f9ca
ThanhVu (Vu) Nguyen @ GiaoChi <20884260+nguyenthanhvuh@users.noreply.github.com>
2015-11-10 11:09:21 -0500

    update paths

--- src/igen_settings.py
+++ src/igen_settings.py
@@ -1,6 +1,6 @@
 tmp_dir = "/var/tmp"
 
-examples_dir = "../examples"
+examples_dir = "../examples/igen"
 otter_dir = "~/igen_exps/otter"
 
 #Note: whenever changing directory, need to recompile coreutils for gcov to work
