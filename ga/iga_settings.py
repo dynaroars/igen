617b286e0ce2e43450491cd9b9f4541ac87b8b14
ThanhVu (Vu) Nguyen @ GiaoChi <20884260+nguyenthanhvuh@users.noreply.github.com>
2015-11-10 15:30:28 -0500

    change ga to iga

--- ga/iga_settings.py
+++
@@ -1,7 +0,0 @@
-tmp_dir = "/var/tmp/iga"
-
-examples_dir = "../examples"
-otter_dir = "~/igen_exps/otter"
-
-#Note: whenever changing directory, need to recompile coreutils for gcov to work
-coreutils_dir = "~/igen_exps/benchmarks/coreutils"  
