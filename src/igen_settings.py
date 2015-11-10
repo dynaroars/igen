23b29c8f2cbb676e967bd85130282de2ecf4dda2
ThanhVu (Vu) Nguyen @ GiaoChi <20884260+nguyenthanhvuh@users.noreply.github.com>
2015-11-10 13:27:51 -0500

    write parse_gcov explicitly (instead of using from get_cov.py

---
+++ src/igen_settings.py
@@ -0,0 +1,8 @@
+tmp_dir = "/var/tmp"
+
+examples_dir = "../examples/igen"
+otter_dir = "~/igen_exps/otter"
+
+#Note: whenever changing directory, need to recompile coreutils for gcov to work
+coreutils_build_dir = "~/igen_exps/coreutils"  
+coreutils_doms_dir = "../benchmarks/doms"
