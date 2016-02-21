f7865401c96ba6b0185e7b493ce59c0a83692e60
ThanhVu (Vu) Nguyen <20884260+nguyenthanhvuh@users.noreply.github.com>
2016-02-20 23:17:56 -0500

    parse xml coverage

---
+++ config/get_cov_python_xml.py
@@ -0,0 +1,70 @@
+#read coverage from files
+
+import vu_common as CM
+def parseFilename(tree, covered, uncovered):
+    assert tree.tag == "class", tree.tag
+    assert set(tree.attrib) == {'branch-rate', 'complexity', 'line-rate', 'name', 'filename'}, tree.attrib
+    
+    filename = tree.attrib['filename']
+    for lines in tree:
+        assert lines.tag in {"methods", "lines"}, lines.tag
+        assert not lines.attrib, lines.attrib
+
+        for line in lines:
+            assert line.tag == "line", line.tag
+            assert set(line.attrib) == set(['hits', 'number']), line.attrib
+
+            lineno = line.attrib["number"]
+            nhits = int(line.attrib['hits'])
+            cline = "{}:{}".format(filename, lineno)
+            if nhits >= 1:
+                covered.add(cline)
+            else:
+                assert nhits == 0, nhits
+                uncovered.add(cline)
+
+def parse(xmlfile):
+    covered = set()
+    uncovered = set()
+    
+    import xml.etree.ElementTree
+    tree = xml.etree.ElementTree.parse(xmlfile)
+    root = tree.getroot()
+    for child in root:
+        assert child.tag == "packages", child.tag
+        assert not child.attrib, child.attrib
+        for child1 in child:
+            assert child1.tag == "package", child1.tag
+            assert set(child1.attrib) == {'branch-rate', 'complexity', 'line-rate', 'name'}, child1.attrib
+            for child2 in child1:
+                assert child2.tag == "classes", child2.tag
+                assert not child2.attrib, child2.attrib
+                for child3 in child2:
+                    parseFilename(child3, covered, uncovered)
+                
+    return covered, uncovered
+
+def cleanup(lines, dirname):
+    """
+    clean up resulting format
+    E.g., dir/file,cover:n  ->  file:n
+    """
+    #remov "/var/tmp/me_prog_cov"
+    lines = set(l.replace(dirname + '/', '') for l in lines)
+    #remove ",cover"
+    lines = set(l.replace(",cover", '') for l in lines)
+    return lines
+    
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("xml", help="xml cov file")
+    args = parser.parse_args()
+
+    covered, uncovered = parse(args.xml)
+    print '{} covered lines'.format(len(covered))
+    #print '\n'.join(sorted(covered))
+
+    print '{} uncovered lines'.format(len(uncovered))
+    #print '\n'.join(sorted(uncovered))
+    
