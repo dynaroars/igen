bc496fdad10de17e2d62134bbd148f9f3080d70c
Ugur <ugurkoc.uk@hotmail.com>
2017-09-25 16:12:16 -0500

    experiment updates

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -10,32 +10,32 @@
 #CoreUtils
 prog="id"
 echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
+python -O $IGEN/src/igen.py ${prog}  --logger_level 2 --seed 0 --benchmark 9 > ~/igen_exps/${prog}_bm9.log 2>&1
 
 prog="uname"
 echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
+python -O $IGEN/src/igen.py ${prog}  --logger_level 2 --seed 0 --benchmark 9 > ~/igen_exps/${prog}_bm9.log 2>&1
 
 prog="mv"
 echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
+python -O $IGEN/src/igen.py ${prog}  --logger_level 2 --seed 0 --benchmark 9 > ~/igen_exps/${prog}_bm9.log 2>&1
 
 prog="cat"
 echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
+python -O $IGEN/src/igen.py ${prog}  --logger_level 2 --seed 0 --benchmark 9 > ~/igen_exps/${prog}_bm9.log 2>&1
 
 prog="ln"
 echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
+python -O $IGEN/src/igen.py ${prog}  --logger_level 2 --seed 0 --benchmark 9 > ~/igen_exps/${prog}_bm9.log 2>&1
 
 prog="date"
 echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
+python -O $IGEN/src/igen.py ${prog}  --logger_level 2 --seed 0 --benchmark 9 > ~/igen_exps/${prog}_bm9.log 2>&1
 
 prog="sort"
 echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
+python -O $IGEN/src/igen.py ${prog}  --logger_level 2 --seed 0 --benchmark 9 > ~/igen_exps/${prog}_bm9.log 2>&1
 
-prog="ls"
-echo ${prog}
-python2.7 igen.py ${prog}  --logger_level 4 --seed 0 --benchmark 20 > ~/Dropbox1/config_results/${prog}_bm20 2>&1
+#prog="ls"
+#echo ${prog}
+#python2.7 igen.py ${prog}  --logger_level 2 --seed 0 --benchmark 9 > ~/igen_exps/${prog}_bm9.log 2>&1
